0443f25997a83739322a5d078642f78c8e190f73
Covija97 <48259969+Covija97@users.noreply.github.com>
2025-03-31 23:02:53 +0200

    Initialize Django app structure with main and meet modules, and remove unused prog module

--- badentracker/badentracker/settings.py
+++ badentracker/badentracker/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    '_main',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
